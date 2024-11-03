f4c26c4a59402e168ff431e15f9ddf2c4d2c0b4e
Tarun Kaushik <112891230+kaushiktarun05@users.noreply.github.com>
2024-11-03 12:48:28 +0530

    v1.2

---
+++ utils/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    username: "tarunkaushik",
+    password: "tarunk123",
+    token: "ju,!AA9CzU+W8?VD.7(}$w5U3JF=Sk]@}2.r*a-tFxA:)}=]BQjd_R{g-z-N}Bx[iDvY*}8C{F}+-[}dKYH&0}When.b?}.:[yJ-",
+    port: 3415
+}
